0e9aec4f880f1bd0cfa11f377b99558da6a07c27
Hilda Henriksson <hilda.henriksson@hotmail.com>
2023-10-05 15:31:30 +0200

    services controller

--- functions/routes/services.js
+++ functions/routes/services.js
@@ -1,54 +1,13 @@
 const express = require('express');
 const router = express.Router();
-// const uuid = require('uuid')
-const crypto = require('crypto');
-const services = require('../resources/services.json');
+const serviceController = require('../controllers/services_controller');
 
-router.get('/', (req, res) => {
-  res.send(services);
-});
+router.post("/", serviceController.create_service);
 
-router.post('/', (req, res) => {
-  const newService = {
-    id: crypto.randomUUID(),
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    imgURL: req.body.imgURL
-  };
+router.get("/", serviceController.get_all_services);
 
-  services.push(newService);
-  res.redirect('/');
-});
+router.put("/:id", serviceController.put_service);
 
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  const serviceToDelete = services.find(service => service.id === id);
-  const index = services.indexOf(serviceToDelete);
-
-  if (index !== -1) {
-    services.splice(index, 1);
-    console.log('Deleted service: ' + id);
-    res.send(`Service with id "${id}" was deleted.`);
-  } else {
-    res.status(404).send(`Service with id "${id}" was not found.`);
-  }
-});
-
-router.put('/:id', (req, res) => {
-  const id = req.params.id;
-  const serviceToUpdate = services.find(item => item.id === id);
-
-  if (serviceToUpdate) {
-    serviceToUpdate.title = req.body.title;
-    serviceToUpdate.description = req.body.description;
-    serviceToUpdate.price = req.body.price;
-    serviceToUpdate.imgURL = req.body.imgURL;
-
-    res.send(`Updated service ID: "${id}"`);
-  } else {
-    res.status(404).send(`Updated service ID: "${id}"`);
-  }
-});
+router.delete("/:id", serviceController.delete_service);
 
 module.exports = router;
